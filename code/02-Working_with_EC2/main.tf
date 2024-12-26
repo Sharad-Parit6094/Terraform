a48209d075d3d9f787b51cc8131d0749c8e3bf0f
Sharad Parit <sharadparit9642@gmail.com>
2024-12-26 18:43:26 +0530

    Update main.tf

--- code/02-Working_with_EC2/main.tf
+++ code/02-Working_with_EC2/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0fd05997b4dff7aac"
   instance_type = "t2.micro"
-  key_name = "PuneKey"
+  key_name = "Pune"
   tags = {
     "Name" = "My instance"
   }
